beb3957aaab7b0442d60cc97657360604e8aae9f
Josh Matthews <josh@jmatthews.us>
2013-11-15 10:15:05 -0500

    Removing user-specific info from reverse-tunnel script.

--- source/downloads/code/rpi-dorm/reverse_tun.sh
+++ source/downloads/code/rpi-dorm/reverse_tun.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#Creates reverse tunnel through sauron when online
+# Creates reverse tunnel through a remote server.
 USER=<Local Username>
 REMOTE_USER=<Remote Username>
 KEY=/home/you/.ssh/tun_rsa
@@ -8,12 +8,12 @@ HOST=<Address of Proxy Server>
 export AUTOSSH_GATETIME=0
 export AUTOSSH_PORT=27554
 
-#ssh options:
-#-f: fork to background
-#-N: don't allocate a terminal
-#-q: quiet
-#-i: path to key file
-#-R: reverse tunnel remoteport:host:localport
-#-S: control socket location, or none
+# ssh options:
+# -f: fork to background
+# -N: don't allocate a terminal
+# -q: quiet
+# -i: path to key file
+# -R: reverse tunnel remoteport:host:localport
+# -S: control socket location, or none
 
 su -c "autossh -f -N -q -i ${KEY} -R ${PORT}:localhost:22 -S none ${REMOTE_USER}@${HOST} -oControlMaster=no -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no" $USER
